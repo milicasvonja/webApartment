4a9c62fa2705f1dc92351abc0c16a5e7128852a2
milicasvonja <milica.svonja997@yahoo.com>
2020-09-10 22:48:03 +0200

    konfiguracija putanja

---
+++ WebProject/src/config/PathConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import java.io.File;
+
+public class PathConfig {
+
+	public static final String FILES = "/files";
+	public static final String USERS_FILE = "/files/Users.json";
+	public static final String APARTMENTS_FILE = "/files/Apartments.json";
+	public static final String COMMENTS_FILE = "/files/Comments.json";
+	public static final String AMENITIES_FILE = "/files/Amenities.json";
+	public static final String APARTMENT_IMAGES = "/files/images/";
+	public static final String RESERVATIONS_FILE = "/files/Reservations.json";
+	public static final String HOLIDAYS_FILE = "/files/Holidays.json";
+}
